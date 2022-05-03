598cb5343c4ff61275ea31da53c62067375cf7d0
Decap <overu2soon@gmail.com>
2022-05-03 17:41:29 +0800

    1.1

--- src/main/java/com/atdecap/springbootbackstage/utils/CodeGenerator.java
+++ src/main/java/com/atdecap/springbootbackstage/utils/CodeGenerator.java
@@ -24,15 +24,16 @@ public class CodeGenerator {
                                 .outputDir("D:\\IdeaProjects\\springbootbackstage\\src\\main\\java\\"); // 指定输出目录
                     })
                     .packageConfig(builder -> {
-                        builder.parent("com/atdecap/springbootbackstage") // 设置父包名
+                        builder.parent("com.atdecap.springbootbackstage") // 设置父包名
                                 .moduleName("") // 设置父包模块名
-                                .pathInfo(Collections.singletonMap(OutputFile.mapper, "D:\\IdeaProjects\\springbootbackstage\\src\\main\\resources\\mapper\\")); // 设置mapperXml生成路径
+                                .pathInfo(Collections.singletonMap(OutputFile.mapper.xml, "D:\\IdeaProjects\\springbootbackstage\\src\\main\\resources\\mapper\\")); // 设置mapperXml生成路径
+
                     })
                     .strategyConfig(builder -> {
                         builder.addInclude("user") // 设置需要生成的表名
                                 .addTablePrefix("t_", "c_"); // 设置过滤表前缀
                     })
-                    .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+//                    .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                     .execute();
         }
 
